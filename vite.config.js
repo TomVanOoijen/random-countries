dbd3e2b741bdbd1e7365d09cb2e5f08ff231da62
Tom van Ooijen <tom@vooijen.info>
2025-02-16 22:29:18 +0100

    feature(folder-structure) added changes to folder structure en sass

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,12 @@ export default defineConfig({
       partialDirectory: "src",
     }),
   ],
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, "public/index.html"),
+  //       about: resolve(__dirname, "public/about/index.html"),
+  //     },
+  //   },
+  // },
 });
